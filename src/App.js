d686d3d68d6a09e9f1fab480d3db92785d812a0e
danielle076 <d.vandenakker@novi-education.nl>
2021-03-02 10:17:57 +0100

    added function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      Schrijf hier jouw functie-component!
-    </div>
-  );
+// Class
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLightOn: true
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            isLightOn: !this.state.isLightOn,
+        });
+    }
+
+    render() {
+        return (
+            <button
+                type="button"
+                onClick={this.handleClick}
+            >
+                {this.state.isLightOn ? 'AAN' : 'UIT'}
+            </button>
+        );
+    }
 }
 
-export default App;
+// Function
+function AppFunction() {
+    const [isLightOn, toggleIsLightOn] = React.useState(true);
+
+    function handleClick() {
+        toggleIsLightOn(!isLightOn);
+    }
+
+    return (
+        <button
+            type="button"
+            onClick={handleClick}
+        >
+            {isLightOn ? "AAN" : "UIT"}
+        </button>
+    );
+}
+
+// Je kan maar 1 export aan hebben staan
+export default AppFunction;
+// export default App;
